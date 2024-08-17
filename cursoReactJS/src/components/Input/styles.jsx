97507ee715368cf38d7cb8769a068ca25a1a36a0
NathanRodrigues59 <nathanrodriguesvieira99@gmail.com>
2024-08-17 13:02:25 -0300

    final estilização input

--- cursoReactJS/src/components/Input/styles.jsx
+++ cursoReactJS/src/components/Input/styles.jsx
@@ -16,6 +16,11 @@ width:100%;
 height:75px;
 background-color: purple;
 border: none;
+padding: 0 10px;
 font-size: 24px;
+display:flex;
+flex-direction:column;
+align-items:flex-end;
+color:#ffffff;
 }
 `;
